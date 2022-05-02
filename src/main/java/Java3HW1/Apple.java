86e677b7a190aacb05e89f9f44562f2f902cab21
AlexeyShimko <shimko390@mail.ru>
2022-05-02 10:55:00 +0300

    Все доделал

--- src/main/java/Java3HW1/Apple.java
+++ src/main/java/Java3HW1/Apple.java
@@ -1,5 +1,7 @@
 package Java3HW1;
 
 public class Apple extends Fruit{
-
+    public Apple() {
+        super(1.0f, "Apple");
+    }
 }
--- src/main/java/Java3HW1/Box.java
+++ src/main/java/Java3HW1/Box.java
@@ -3,5 +3,39 @@ package Java3HW1;
 import java.util.ArrayList;
 
 public class Box<T extends Fruit> {
+    private  ArrayList<T> fruits;
 
+    public Box(){
+        fruits = new ArrayList<>();
+    }
+
+    public ArrayList<T> getFruits(){
+        return fruits;
+    }
+
+    public void addInBox(T fruit){
+        fruits.add(fruit);
+    }
+
+    public float getWeight(){
+        float sumWeight = 0.0f;
+
+        for (T fruit: fruits){
+            sumWeight += fruit.getWeight();
+        }
+        return sumWeight;
+    }
+
+    public void addFruits(ArrayList<T> fruit){
+        this.fruits.addAll(fruit);
+    }
+
+    public void  moveAllToAnotherBox(Box<T> box){
+        box.addFruits(getFruits());
+        fruits.clear();
+    }
+
+    public boolean compare(Box<?> box){
+        return Math.abs(this.getWeight() - box.getWeight()) < 0.001;
+    }
 }
